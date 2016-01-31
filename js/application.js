ddcc7a60575ff6d841d4e96175aba6ee6c1d0f59
cmtaubman <carmen@cgkcorporation.com>
2016-01-31 19:41:49 +0800

    trying to make timer work

--- js/application.js
+++ js/application.js
@@ -4,33 +4,56 @@ $(document).ready(function(){
 
 // count-down clock. Initial value is 10 seconds
 
-var count=10;
 
-var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+var count=10;
+var $secondsLeft = $("#secondsLeft");
+var $solutionInput = $("#solution-input");
+
+
+//to start the game
+$solutionInput.keydown(function() {
+  counter = setInterval(time, 1000);
+  console.log("hey");
+});
+
+function time() {
+  count--
+  $secondsLeft.text(count);
+  if (count === 0) {
+    clearInterval(counter);
+  }
+};
+
+time();
+
+//MY PREVIOUS TIMER FUNCTION
+// var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
+// function timer() {
+//   count--;
+//     if (count <=0) {
+//       clearInterval(counter);
+//     }
+// }
+
+// $("#secondsLeft").text(count);
+
+// timer();
 
 // When the user inputs any character in the solution input: isNumeric(1) returns Boolean value true if there is a number
 // Timer will start counting down
 
-var solutionInputCheck
-  if ($("#solution-input")>='0') {
-  return true;
-  } else {
-    return false;
-  }
 
-if ($("#solution-input") = true) {
-  function timer() {
-  count = count-1;
-    if (count <= -1) {
-     clearInterval(counter);
-     //counter ended, do something here
-     return;
-    }
-  }
-
-$("#secondsLeft").text(count);
   // Do code for showing the number of seconds here
-}
+
+
+
+// var startGame = function(){
+//     $("#solution-input").numeric()
+//       if ($(this)==1) {
+//       timer ();
+//     }
+//   };
 
 
 // random math problem to solve. Initial operation is addition only: ie. (2 + 5 = <input>)
@@ -41,19 +64,21 @@ var number1 = $("#num1").text(1+ Math.floor(Math.random() *10));
 var number2 = $("#num2").text(1+ Math.floor(Math.random() *10));
 // provides a random number between 1 and 10
 
-//User to input answer and press enter to submit. I know I need to find the char code for "enter" and input this here
-var inputAnswer = $('#solution-input').val()
+//User to input answer
 
+var input = $("#solution-input").val();
 
-//solution checker variable
+var solutionAnswer= ($("#num1")+$("#num2"));
 
-// var solutionCheck
-//   if ($('#solution-input').val = $('num1'+'num2')) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+function checkAnswer (num1, num2, input) {
+  var solution = num1+num2;
+  if (input===solution) {
+    console.log("woohoo");
+  } else console.log("toobad");
+}
+
+checkAnswer(num1, num2, $("#solution-input").val());
+
 
 // Check the user's answer against the solution:
 
